8b50f9d674faa8cd738c78bc4d64d9337b7ebb3d
fgomeza17 <46194896+fgomeza17@users.noreply.github.com>
2025-07-06 21:58:01 -0400

    Update screen.c

--- boards/shields/nice_oled/widgets/screen.c
+++ boards/shields/nice_oled/widgets/screen.c
@@ -225,13 +225,13 @@ int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent) {
     lv_obj_align(zmk_widget_luna_obj(&luna_widget), LV_ALIGN_TOP_LEFT, 50, 0);
 #endif
 
-#if IS_ENABLED(CONFIG_NICE_OLED_WIDGET_HID_INDICATORS)
-    zmk_widget_hid_indicators_init(&hid_indicators_widget, canvas);
-#endif
+// #if IS_ENABLED(CONFIG_NICE_OLED_WIDGET_HID_INDICATORS)
+//     zmk_widget_hid_indicators_init(&hid_indicators_widget, canvas);
+// #endif
 
-#if IS_ENABLED(CONFIG_NICE_OLED_WIDGET_MODIFIERS_INDICATORS)
-    zmk_widget_modifiers_init(&modifiers_widget, canvas); // Inicializar el widget de modifiers
-#endif
+// #if IS_ENABLED(CONFIG_NICE_OLED_WIDGET_MODIFIERS_INDICATORS)
+//     zmk_widget_modifiers_init(&modifiers_widget, canvas); // Inicializar el widget de modifiers
+// #endif
 
     return 0;
 }
